0f057e07fed5150ef8fe9e0d554fa6355c85f009
DennisBalk <dennis.balk@justedit.com>
2023-03-14 09:03:52 +0100

    fix sound.js

--- js/sound.js
+++ js/sound.js
@@ -55,7 +55,7 @@ export default class Sound {
 				return this.letterizer.letterizeZwanzigEinsNumber(number);
 				
 			case 'traditionellVerdreht':
-				return this.letterizer.letterizeVerdreht(number);
+				return number;
 				
 			case 'zwanzigeinsEndnull':
 				return this.letterizer.letterizeZwanzigEinsNumberEndnull(number);
