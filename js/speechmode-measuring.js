51e5896daa67239d6b044f8ab7948d92e5abe179
Lukas Glowania <lukas.glowania@justedit.com>
2024-01-15 13:26:31 +0100

    add speechmode-measuring-tool

---
+++ js/speechmode-measuring.js
@@ -0,0 +1,100 @@
+import Letterizer from "./letterizer.js";
+
+function playWord(word, finishedHandler) {
+
+	var msg = new SpeechSynthesisUtterance(word);
+
+	msg.onend = finishedHandler;
+	msg.lang = 'de-DE';
+	window.speechSynthesis.speak(msg);
+}
+
+let letterizer = new Letterizer();
+
+let startButton = document.querySelector('button');
+
+let iterationsElem = document.querySelector('#iterations');
+let durationTraditionellVerdrehtElem = document.querySelector('#durationTraditionellVerdreht');
+let durationZehneinsElem = document.querySelector('#durationZehneins');
+
+let measurementsCountInput = document.querySelector('input');
+
+let randomNumberPoolFloor = 10011;
+let randomNumberPoolCeil = 99999;
+
+let resultMillis;
+let currentDurationOutput = durationTraditionellVerdrehtElem;
+
+let maxMeasurementsCount;
+
+let randomNumbers;
+
+startButton.onclick = evt => {
+	
+	randomNumbers = [];
+	
+	maxMeasurementsCount = measurementsCountInput.value;
+
+	for(let i = 0; i < maxMeasurementsCount; i++){
+		
+		let number = randomIntFromInterval(randomNumberPoolFloor, randomNumberPoolCeil);
+		randomNumbers.push(number);
+	}
+	
+	resultMillis = 0;
+	takeMeasurement(0, true);
+};
+
+function takeMeasurement(iteration, traditionellVerdrehtEnabled) {
+	
+	if(iteration >= maxMeasurementsCount) {
+		
+		if(traditionellVerdrehtEnabled) {
+			
+			traditionellVerdrehtEnabled = false;
+			iteration = 0;
+			resultMillis = 0;
+			currentDurationOutput = durationZehneinsElem;
+		}
+		else {
+			return;
+		}
+	}
+	
+	let number = randomNumbers[iteration];
+		
+	let utteranceOutput;
+	
+	if(traditionellVerdrehtEnabled) {
+		utteranceOutput = letterizer.letterizeTraditionellVerdrehtNumber(number);
+	}
+	else {
+		utteranceOutput = letterizer.letterizeZehnEinsNumber(number);
+	}
+	
+	let nowMillis = new Date().getTime();
+	
+	playWord(utteranceOutput, () => {
+	
+		let finishedMillis = new Date().getTime();
+		let ellapsedMillis = finishedMillis - nowMillis;
+		resultMillis += ellapsedMillis;
+		
+		iterationsElem.textContent = iteration + 1;
+		currentDurationOutput.textContent = millisToMinutesAndSeconds(resultMillis) + ' (' + resultMillis + 'ms)';
+		
+		takeMeasurement(iteration + 1, traditionellVerdrehtEnabled);
+	});
+}
+
+function randomIntFromInterval(min, max) { // min and max included
+ 
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function millisToMinutesAndSeconds(millis) {
+	
+  var minutes = Math.floor(millis / 60000);
+  var seconds = ((millis % 60000) / 1000).toFixed(0);
+  return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+}
